493e693575c653ee99d4c3fc82ec0ef199c8efc7
Zun Liang <107293493+zun-liang@users.noreply.github.com>
2024-06-11 20:44:33 -0500

    fix: type errors

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,7 @@ import { Margin, Data } from "./types";
 
 const width: number = 920;
 const height: number = 630;
-const margin: Margin = { top: 60, right: 20, bottom: 80, left: 70 };
-
-const parseTime = (time: string) =>
-  `${time.split(":")[0]}.${time.split(":")[1]}`;
+const margin: Margin = { top: 80, right: 70, bottom: 80, left: 70 };
 
 fetch(
   "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json"
@@ -17,15 +14,12 @@ fetch(
     const xScale: d3.ScaleLinear<number, number> = d3
       .scaleLinear()
       .domain(d3.extent(data, (d) => d.Year) as [number, number])
-      .nice()
-      .range([margin.left, width - margin.left]);
+      .range([margin.left, width - margin.left])
+      .nice();
 
-    const yScale: d3.ScaleLinear<number, number> = d3
-      .scaleLinear()
-      .domain(
-        d3.extent(data, (d) => Number(parseTime(d.Time))) as [number, number]
-      )
-      .nice()
+    const yScale: d3.ScaleTime<number, number> = d3
+      .scaleTime()
+      .domain(d3.extent(data, (d) => d3.timeParse("%M:%S")(d.Time)) as [Date, Date])
       .range([margin.top, height - margin.bottom]);
 
     const svg = d3
@@ -34,13 +28,15 @@ fetch(
       .attr("viewBox", `0 0 ${width} ${height}`)
       .classed("svg-content", true);
 
+    const tooltip = d3.select("body").append("div").attr("id", "tooltip");
+
     svg
       .append("text")
       .attr("id", "title")
       .attr("x", width / 2)
-      .attr("y", 60)
+      .attr("y", 30)
       .attr("text-anchor", "middle")
-      .style("font-size", "24px")
+      .style("font-size", "30px")
       .style("fill", "black")
       .text("Doping in Professional Bicycle Racing");
 
@@ -48,9 +44,9 @@ fetch(
       .append("text")
       .attr("id", "subtitle")
       .attr("x", width / 2)
-      .attr("y", 80)
+      .attr("y", 60)
       .attr("text-anchor", "middle")
-      .style("font-size", "16px")
+      .style("font-size", "20px")
       .style("fill", "black")
       .text("35 Fastest times up Alpe d'Huez");
 
@@ -58,13 +54,16 @@ fetch(
       .append("g")
       .attr("id", "x-axis")
       .attr("transform", `translate(0, ${height - margin.bottom})`)
-      .call(d3.axisBottom(xScale));
+      .call(d3.axisBottom(xScale).tickFormat(d3.format("d")));
 
-    svg
+      svg
       .append("g")
       .attr("id", "y-axis")
       .attr("transform", `translate(${margin.left}, 0)`)
-      .call(d3.axisLeft(yScale));
+      .call(d3.axisLeft(yScale).tickFormat(d => {
+        const date = new Date(d.valueOf());
+        return d3.timeFormat("%M:%S")(date);
+      }));
 
     svg
       .selectAll("circle")
@@ -73,19 +72,35 @@ fetch(
       .append("circle")
       .attr("class", "dot")
       .attr("cx", (d) => xScale(d.Year))
-      .attr("cy", (d) => yScale(Number(parseTime(d.Time))))
+      .attr("cy", (d) => yScale(d3.timeParse("%M:%S")(d.Time)!))
       .attr("data-xvalue", (d) => d.Year)
-      .attr("data-yvalue", (d) => Number(parseTime(d.Time)))
+      .attr("data-yvalue", (d) => {
+        const parsedTime = d3.timeParse("%M:%S")(d.Time);
+        return parsedTime ? parsedTime.toISOString() : "";
+      })
       .attr("r", 5)
-      .attr("fill", "blue");
+      .attr("fill", (d) => (d.Doping ? "steelblue" : "orange"))
+      .attr("stroke", "black")
+      .attr("stroke-width", "1px")
+      .on("mouseover", (event, d) => {
+        tooltip
+          .style("display", "block")
+          .html(
+            `${d.Name}: ${d.Nationality}<br/>Year: ${d.Year}, Time: ${d.Time}<br/>${d.Doping}`
+          )
+          .attr("data-year", d.Year)
+          .style("left", `${event.pageX + 10}px`)
+          .style("top", `${event.pageY + 10}px`);
+      })
+      .on("mouseout", () => tooltip.style("display", "none"));
 
     svg
       .append("text")
       .attr("transform", "rotate(-90)")
       .attr("x", -height / 3)
-      .attr("y", 30)
+      .attr("y", 20)
       .text("Time in Minutes")
-      .style("font-size", "15px");
+      .style("font-size", "18px");
 
     const legend = svg
       .append("g")
@@ -94,9 +109,9 @@ fetch(
 
     const categories = [
       { label: "No doping allegations", color: "orange" },
-      { label: "Riders with doping allegations", color: "blue" },
+      { label: "Riders with doping allegations", color: "steelblue" },
     ];
-    
+
     categories.forEach((category, i) => {
       const legendRow = legend
         .append("g")
